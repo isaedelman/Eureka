afef76a7ef9d4ff6b456fc266bdd2efded1c12c0
Aarynn Carter <23636747+AarynnCarter@users.noreply.github.com>
2022-09-19 16:35:45 -0700

    Adding data conversion functionality for: Firefly, Michael Radica's SOSS, and Eureka! with no meta provided file, reductions.

--- src/eureka/S4_generate_lightcurves/data_convert.py
+++ src/eureka/S4_generate_lightcurves/data_convert.py
@@ -5,8 +5,8 @@
 import glob, os
 import numpy as np
 import astraeus.xarrayIO as xrio
+from astropy.io import fits
 from copy import copy
-import time as t
 
 def convert_to_eureka(meta):
     '''Convert provided data into Eureka specific format
@@ -52,13 +52,15 @@ def convert_to_eureka(meta):
             convert = convert_firefly
         elif meta.format == 'radica_soss':
             convert = convert_radica_soss
+        elif meta.format == 'eureka_nometa':
+            convert = convert_eureka_nometa
 
         # Call the conversion function on the file
-        spec = convert(file, savefile)
+        spec = convert(meta, file, savefile)
 
     return meta, spec
 
-def convert_exotic_jedi(file, savefile):
+def convert_exotic_jedi(meta, file, savefile):
     '''Convert ExoTiC-JEDI data to Eureka! data
 
     Parameters
@@ -86,18 +88,135 @@ def convert_exotic_jedi(file, savefile):
     # Create x-axis array
     x = np.arange(len(wave))
 
+    #Gather drifts
+    xcen = raw_spec['x_shift'].values
+    ycen = raw_spec['y_shift'].values
+
     # Construct and save the dataset in Eureka! format
-    spec = assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile)
+    spec = assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile,
+                                    xcen=xcen, ycen=ycen)
 
     return spec
 
-def convert_firefly(file, savefile):
-    return
+def convert_firefly(meta, file, savefile):
+    '''Convert FireFly PRISM data to Eureka! data
 
-def convert_radica_soss(file, savefile):
-    return
+    Parameters
+    ----------
+    meta : eureka.lib.readECF.MetaClass
+        The current metadata object.
+    file : str
+        String identifier for the data file
+    savefile : str
+        String identifier for where to save the converted file
 
-def assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile):
+    Returns
+    -------
+    spec : object
+        Xarray Dataset containing saved information.
+    '''
+
+    data = np.load(file, allow_pickle=True).item()
+    # Assign things
+    time = data['time']
+    wave = data['wavelength']
+    specs = data['specphot']
+    err = data['specphot_err']    
+    # Create x-axis array
+    x = np.arange(len(wave))
+    # Create a empty mask to satisfy Eureka!
+    mask = np.zeros_like(specs)
+
+    #Gather drifts
+    xcen = data['shx']
+    ycen = data['shy']
+
+    # Construct and save the dataset in Eureka! format
+    spec = assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile, \
+                                    xcen=xcen, ycen=ycen)
+
+    return spec
+
+def convert_radica_soss(meta, file, savefile):
+    '''Convert Michael Radica's SOSS data to Eureka! data
+
+    Parameters
+    ----------
+    meta : eureka.lib.readECF.MetaClass
+        The current metadata object.
+    file : str
+        String identifier for the data file
+    savefile : str
+        String identifier for where to save the converted file
+
+    Returns
+    -------
+    spec : object
+        Xarray Dataset containing saved information.
+    '''
+    with fits.open(file) as hdu:
+        print(hdu.info())
+        # Grab time
+        time = hdu['Time'].data - 2400000.5
+
+        if hasattr(meta, 'soss_order'):
+            if meta.soss_order not in [1,2]:
+                raise ValueError('Specified NIRISS SOSS order must either be '+
+                    '"1" or "2"')
+        else:
+            meta.soss_order = 1
+        print(f'Extracting Order {meta.soss_order} of NIRISS SOSS data')
+
+        # Grab the Order info
+        wave_lo = hdu['Wave Low O{}'.format(meta.soss_order)].data[0]
+        wave_up = hdu['Wave Up O{}'.format(meta.soss_order)].data[0]
+        wave = (wave_lo + wave_up) / 2
+        specs = hdu['Flux O{}'.format(meta.soss_order)].data
+        err = hdu['Flux Err O{}'.format(meta.soss_order)].data
+    
+    # Create x-axis array
+    x = np.arange(len(wave))
+    # Create a empty mask to satisfy Eureka!
+    mask = np.zeros_like(specs)
+
+    # Fix NaN's so they don't break the code
+    specs[np.where(np.isnan(specs))] = 0
+    err[np.where(np.isnan(specs))] = 0
+    mask[np.where(np.isnan(specs))] = 1
+
+    # All arrays are backwards, so lets flip them
+    wave = np.flip(wave)
+    specs = np.flip(specs, axis=1)
+    err = np.flip(err, axis=1)
+
+    # # Construct and save the dataset in Eureka! format
+    spec = assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile)
+
+    return spec 
+
+def convert_eureka_nometa(meta, file, savefile):
+    '''Wrapper to read in Eureka! data when no meta data was provided / saved
+
+    Parameters
+    ----------
+    meta : eureka.lib.readECF.MetaClass
+        The current metadata object.
+    file : str
+        String identifier for the data file
+    savefile : str
+        String identifier for where to save the converted file
+
+    Returns
+    -------
+    spec : object
+        Xarray Dataset containing saved information.
+    '''
+    spec = xrio.readXR(file)
+
+    return spec 
+
+def assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile, \
+                            xcen=np.array(False), ycen=np.array(False)):
     '''Assemble a Eureka! compatible dataset
 
     Parameters
@@ -128,6 +247,7 @@ def assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile):
     # Assemble the dataset
     spec = xrio.makeDataset()
     spec['time'] = time
+    spec['time'].attrs['time_units'] = time_units
     spec['x'] = x
     spec['optspec'] = (['time', 'x'], specs)
     spec['optspec'].attrs['flux_units'] = flux_units
@@ -142,8 +262,13 @@ def assemble_eureka_dataset(time, x, specs, err, mask, wave, savefile):
     spec['optmask'].attrs['time_units'] = time_units
     spec['optmask'].attrs['wave_units'] = wave_units
     spec['wave_1d'] = (['x'], wave)
-    spec['wave_1d'].attrs['wave_units'] = 'microns'
-    
+    spec['wave_1d'].attrs['wave_units'] = wave_units
+
+    if xcen.any():
+        spec['centroid_x'] = (['time'], xcen)
+    if ycen.any():
+        spec['centroid_y'] = (['time'], ycen)
+
     # Save this file so we don't need to repeat things
     xrio.writeXR(savefile, spec)
 
